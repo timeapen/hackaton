3b16bf20752c7ae96d1af265e958f8e0ef636613
wr0bel <tomasz.wrobel@gmail.com>
2017-01-03 14:00:31 -0500

    Initial commit

---
+++ aavengers/src/main/java/com/aavengers/AavengersApplication.java
@@ -0,0 +1,12 @@
+package com.aavengers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AavengersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AavengersApplication.class, args);
+	}
+}
