063857f1abbe9fff79873e321e0153421f17ba03
Tim Eapen <eapent@intware.com>
2017-01-04 15:43:35 -0500

    TE - pulling out radar data into json file. Filling in index and index ratings in controller

--- aavengers-ui/src/app/charts/radar.js
+++ aavengers-ui/src/app/charts/radar.js
@@ -1,175 +1,19 @@
 class RadarController {
   /** @ngInject */
-  constructor($log) {
+  constructor($http, $log) {
     $log.info('RADAR');
-    /*
-    this.chartData = {
-      type: "line",
-      series: [
-        {values: [54, 23, 34, 23, 43]},
-        {values: [10, 15, 16, 20, 40]}
-      ]
-    };
-    */
-    this.chartData = {
-      "type": "radar",
-      "background-color": "white",
-      "border-top": "3px solid #7e7e7e",
-      "border-bottom": "3px solid #7e7e7e",
-      "border-right": "3px solid #7e7e7e",
-      "border-left": "3px solid #7e7e7e",
-      "border-color": "black",
-      "border-size": "5",
-      "title": {
-        "text": "City Hospital Performance",
-        "background-color": "none",
-        "font-color": "000",
-        "font-size": "22px"
-      },
-      "tooltip": {
-        "text": "%t<br>%k Is %v",
-        "shadow": 0,
-        "border-radius": 3
-      },
-      "scale-k": {
-        "background-color": "none",
-        "values": [
-          "Quality",
-          "Efficiency",
-          "Satisfaction",
-          "Value",
-          "Cost"
-        ],
-        "item": {
-          "font-size": "14px",
-          "padding-left": "30px",
-          "padding-bottom": "15px"
-        },
-        "guide": {
-          "line-color": "#818181",
-          "line-style": "solid",
-          "line-width": "2px",
-          "items": [
-            {
-              "background-color": "#fff"
-            }
-          ]
-        },
-        "tick": {
-          visible: false
-        }
-      },
-      "scale-v": {
-        "values": [
-          "Poor",
-          "Fair",
-          "Good",
-          "Very Good",
-          "Excellent"
-        ],
-        "ref-line": {
-          "line-width": "1px",
-          "line-color": "#818181"
-        },
-        "guide": {
-          "line-width": ".5px",
-          "line-style": "dashed"
-        },
-        "tick": {
-          "size": 10,
-          "line-width": ".5px",
-          "line-length": 0.55,
-          "line-color": "#818181"
-        },
-        "item": {
-          "padding-left": "9.5px",
-          "font-size": "8px"
-        }
-      },
-      "series": [
-        {
-          "values": [
-            "Fair",
-            "Poor",
-            "Good",
-            "Very Good",
-            "Good"
-          ],
-          "aspect": "line",
-          "text": "ER",
-          "line-color": "#999999",
-          "background-color": "none",
-          "line-width": "4px",
-          "alpha": "0.35",
-          "marker": {
-            "background-color": "#999999",
-            "size": "4",
-            "border-color": "#999999",
-            "alpha": "0.55"
-          }
-        },
-        {
-          "values": [
-            "Fair",
-            "Poor",
-            "Excellent",
-            "Excellent",
-            "Good"
-          ],
-          "aspect": "line",
-          "text": "ENT",
-          "line-color": "#666666",
-          "line-width": "4px",
-          "alpha": "0.85",
-          "marker": {
-            "background-color": "#666666",
-            "size": "4",
-            "border-color": "#666666",
-            "alpha": "0.55"
-          }
-        },
-        {
-          "values": [
-            "Very Good",
-            "Excellent",
-            "Very Good",
-            "Good",
-            "Excellent"
-          ],
-          "aspect": "line",
-          "text": "O&G",
-          "line-color": "#BBDFFF",
-          "line-width": "4px",
-          "alpha": "0.45",
-          "marker": {
-            "background-color": "#BBDFFF",
-            "size": "4",
-            "border-color": "#BBDFFF",
-            "alpha": "0.55"
-          }
-        },
-        {
-          "values": [
-            "Poor",
-            "Very Good",
-            "Good",
-            "Good",
-            "Very Good"
-          ],
-          "aspect": "line",
-          "text": "Pharmacy",
-          "line-color": "#6fbbff",
-          "line-width": "4px",
-          "alpha": "0.85",
-          "marker": {
-            "background-color": "#6fbbff",
-            "size": "4",
-            "border-color": "#6fbbff",
-            "alpha": "0.55"
-          }
-        }
-      ]
-    };
+
+    $http
+      .get('app/charts/radar.json')
+      .then(response => {
+        this.chartData = response.data;
+
+        const gaiaIndices = ["Quality", "Efficiency", "Satisfaction", "Value", "Cost"];
+        this.chartData["scale-k"].values = gaiaIndices;
+
+        const gaiaIndexRatings = ["Poor", "Fair", "Good", "Very Good", "Excellent"];
+        this.chartData["scale-v"].values = gaiaIndexRatings;
+      });
   }
 }
 
